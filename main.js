74b6a30c952de5f48a38a5ce67e4e231ea72ada0
DgSantanaBR <diiiego.santana2@gmail.com>
2023-05-11 12:15:06 -0300

    Inicio do protifolio no github

---
+++ main.js
@@ -0,0 +1,26 @@
+//  script em JS que adiciona uma classe "active" ao link do menu atual
+const currentLocation = location.href;
+const menuItems = document.querySelectorAll('nav a');
+const menuLength = menuItems.length;
+
+for (let i = 0; i < menuLength; i++) {
+  if (menuItems[i].href === currentLocation) {
+    menuItems[i].className = 'active';
+  }
+}
+
+
+window.addEventListener("scroll", function()  {
+  const conteudo = document.getElementById("menu");
+  const limite = conteudo.offsetTop + conteudo.clientHeight;
+  if (window.pageYOffset >= conteudo.offsetTop && window.pageYOffset > limite) {
+    conteudo.classList.add("modMenu");
+    
+  } else {
+
+    conteudo.classList.remove("modMenu");
+    
+  }
+});
+    
+   
